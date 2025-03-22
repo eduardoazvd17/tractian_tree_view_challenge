fb47f4f3cde51778ca8a550312386a65cf20da29
Eduardo Azevedo <eduardoazvd17@gmail.com>
2025-03-22 09:06:54 -0300

    config: set app bar theme

--- lib/src/core/theme/theme.dart
+++ lib/src/core/theme/theme.dart
@@ -3,15 +3,24 @@ import 'package:flutter/material.dart';
 ThemeData get lightTheme => ThemeData(
   colorScheme: ColorScheme.fromSeed(
     seedColor: Colors.blue,
+    surface: Colors.white,
     brightness: Brightness.light,
   ),
+  appBarTheme: _appBarTheme,
   useMaterial3: true,
 );
 
 ThemeData get darkTheme => ThemeData(
   colorScheme: ColorScheme.fromSeed(
     seedColor: Colors.blue,
+    surface: Colors.black,
     brightness: Brightness.dark,
   ),
+  appBarTheme: _appBarTheme,
   useMaterial3: true,
+);
+
+AppBarTheme get _appBarTheme => AppBarTheme(
+  backgroundColor: Color(0xff17192D),
+  foregroundColor: Colors.white,
 );
